618552d67cda89735ffe71377f9f12bc9b859323
Vishwas <vishwas24fps@gmail.com>
2021-04-06 12:25:08 +0530

    clean some code

--- shopping/views.py
+++ shopping/views.py
@@ -19,24 +19,10 @@ def index(request):
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - n // 4)
         allProds.append([prod,range(1,nSlides), nSlides])
-    # params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
-    # allprods = [[products, range(1, nSlides), nSlides],
-    #            [products, range(1, nSlides), nSlides]]
     params = {'allprods':allProds}
     return render(request, 'layout/index.html', params)
-# def index(request):
-#     products= Product.objects.all()
-#     allProds=[]
-#     catprods= Product.objects.values('category', 'id')
-#     cats= {item["category"] for item in catprods}
-#     for cat in cats:
-#         prod=Product.objects.filter(category=cat)
-#         n = len(prod)
-#         nSlides = n // 4 + ceil((n / 4) - (n // 4))
-#         allProds.append([prod, range(1, nSlides), nSlides])
-#
-#     params={'allProds':allProds }
-#     return render(request,"layout/index.html", params)
+
+
 def about(request):
     return render(request, 'layout/about.html')
 
